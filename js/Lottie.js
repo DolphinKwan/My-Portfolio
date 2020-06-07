ca6b6e6c9b7763b17bbc9f8a39bcb5e81c2d3a10
Daoxun Guan <Dolphin_Kwan@hotmail.com>
2020-06-07 16:57:45 -0500

    Moon phases & Weather

--- js/Lottie.js
+++ js/Lottie.js
@@ -38,6 +38,22 @@ var animation1_1 = bodymovin.loadAnimation({
     path: 'data2_2.json'
   })
 
+  var animation2_3 = bodymovin.loadAnimation({
+    container: document.getElementById('bm2_3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'data2_3.json'
+  })
+
+  var animation3_1 = bodymovin.loadAnimation({
+    container: document.getElementById('bm3_1'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'data3_1.json'
+  })
+
 function firstQuarter() {
   $(function(){
     "use strict";
@@ -75,14 +91,84 @@ function thirdQuarter() {
 }
   
 function newMoon() {
+
+
   $(function(){
     "use strict";
-
+    
     animation2_2.playSegments([0,240], true);
 
     $("#moonphase").html("New Moon");
 
   })
   
+}
+function firstQuarterMoonrise() {
+  $(function(){
+    "use strict";
+    animation2_3.playSegments([576,831], true);
 
+    // $("#moonphase").html("First Quarter");
+  })
+  
+}
+function fullMoonMoonrise() {
+
+  $(function(){
+    "use strict";
+
+    animation2_3.playSegments([1231,1594], true);
+
+    // $("#moonphase").html("Full Moon");
+
+  })
+  
+
+}
+
+function thirdQuarterMoonrise() {
+  $(function(){
+    "use strict";
+
+    animation2_3.playSegments([1913,2201], true);
+
+    // $("#moonphase").html("Third Quarter");
+
+  })
+  
+
+}
+  
+function newMoonMoonrise() {
+  $(function(){
+    "use strict";
+
+    animation2_3.playSegments([0,235], true);
+
+    // $("#moonphase").html("New Moon");
+
+  })
+}
+
+function cloudy() {
+  $(function(){
+    "use strict";
+    animation3_1.setDirection(-1)
+    animation3_1.play();
+
+
+    // $("#moonphase").html("New Moon");
+
+  })
+}
+
+function foggy() {
+  $(function(){
+    "use strict";
+    animation3_1.setDirection(1)
+    animation3_1.play();
+
+    // $("#moonphase").html("New Moon");
+
+  })
 }
